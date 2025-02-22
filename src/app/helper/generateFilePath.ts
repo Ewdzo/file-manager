60f90a3b15ff998f19bc4182c954b65afae825c0
Enzo <enzo.amorim@vilt-group.com>
2025-02-22 10:25:24 -0300

    ✨ add generateFilePath

---
+++ src/app/helper/generateFilePath.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+export const generateFilePath: any = (
+  folder: string,
+  fileName: string,
+) => {
+  const pathToCheck = folder + "/" + fileName;
+
+  if (!fs.existsSync(pathToCheck)) return pathToCheck;
+
+  const hasNumber = fileName.match(/\d+/);
+
+  if (hasNumber) {
+    const number = Number(hasNumber[0]) + 1;
+    const newFileName = fileName.split(hasNumber[0]).join(number + "");
+
+    return generateFilePath(folder, newFileName, number);
+  }
+
+  const name = fileName.split(".");
+  const newFileName = name[0] + "_1." + name[1];
+
+  return generateFilePath(folder, newFileName);
+};
